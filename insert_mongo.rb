d8ff80fff2bdf816fca16c36b8c6fb95ed3ad33d
SUMANTH PIKKILI <sumanthpikkili@SUMANTHs-MacBook-Pro-3.local>
2015-12-04 18:42:31 -0600

    sixth

--- insert_mongo.rb
+++ insert_mongo.rb
@@ -2,11 +2,11 @@ require 'mongo'
 require 'csv'
 
 
-mongo_uri = 'mongodb://heroku_8c77z5bn:s650vnnnpv39p3h8epm0jima98@ds053764.mongolab.com:53764/heroku_8c77z5bn'
+mongo_uri = 'mongodb://heroku_1lfh5b08:k8di7ss33j6lndhcg76j87jd27@ds063134.mongolab.com:63134/heroku_1lfh5b08'
 
 begin
     #Connecting to the Mongo Client
-    client = Mongo::Client.new([ 'ds053764.mongolab.com:53764' ],:database =>'heroku_8c77z5bn',:user=>'heroku_8c77z5bn',:password=>'s650vnnnpv39p3h8epm0jima98');
+    client = Mongo::Client.new([ 'ds063134.mongolab.com' ],:database =>'heroku_1lfh5b08',:user=>'heroku_1lfh5b08',:password=>'k8di7ss33j6lndhcg76j87jd27');
     puts "Successfully connected to mongo db"
     db = client.database
     db.collection_names.each{|name| puts name }
