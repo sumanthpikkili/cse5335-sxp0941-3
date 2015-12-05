2bf0e288594625b2e688cda048e1596fda52cfe7
SUMANTH PIKKILI <sumanthpikkili@SUMANTHs-MacBook-Pro-3.local>
2015-12-04 18:34:07 -0600

    fifth

--- retrieve_mongo.rb
+++ retrieve_mongo.rb
@@ -1,23 +1,14 @@
 require 'mongo'
 require 'csv'
 
-mongo_uri = 'mongodb://heroku_8c77z5bn:s650vnnnpv39p3h8epm0jima98@ds053764.mongolab.com:53764/heroku_8c77z5bn'
+mongo_uri = 'mongodb://heroku_1lfh5b08:k8di7ss33j6lndhcg76j87jd27@ds063134.mongolab.com:63134/heroku_1lfh5b08'
 
 begin
-    client = Mongo::Client.new([ 'ds053764.mongolab.com:53764' ],:database =>'heroku_8c77z5bn',:user=>'heroku_8c77z5bn',:password=>'s650vnnnpv39p3h8epm0jima98');
-    puts "Successfully connected to mongo db"
-    puts "Enter the primary key value"
-    pk = gets
-    if pk.strip! == nil
-	pkey=pk
-    else
-	pkey=pk
-    end
+    client = Mongo::Client.new([ 'ds063134.mongolab.com:63134' ],:database =>'heroku_1lfh5b08',:user=>'heroku_1lfh5b08',:password=>'k8di7ss33j6lndhcg76j87jd27');   
     db = client.database
     college=db.collection('universities')
     puts "Retrieve successful"
-    puts "The retrieved data for the primary key value entered is"
-    result = college.find({ :unitid =>pkey })
+    result = college.find()
     puts "UNITID | OPEID | OPEID6 | INSTNM | CITY | STABBR | INSTURL"              
     result.each do |document|
   	puts "#{document['unitid']} | #{document['opeid']} | #{document['opeid6']} | #{document['instnm']} | #{document['city']} | #{document['stabbr']} | #{document['insturl']}" 
